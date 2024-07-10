1c20d2cfb81ee5a7648f18c280b53df17cd937fc
bzmariano <48970081+bzmariano@users.noreply.github.com>
2024-07-10 00:38:44 -0300

    update wezterm conf

--- wezterm.lua
+++ wezterm.lua
@@ -3,41 +3,40 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- COLORSCHEME
-config.color_scheme = "tokyonight-night"
-local foreground_color = "silver"
-local background_color = "#101010"
+config.color_scheme = "tokyonight"
+local fg = "silver"
+local bg = "#101515"
 config.colors = {
-	foreground = foreground_color,
-	background = background_color,
+	foreground = fg,
+	background = bg,
 	tab_bar = {
 		active_tab = {
-			fg_color = foreground_color,
-			bg_color = background_color,
+			fg_color = fg,
+			bg_color = bg,
 			intensity = "Normal",
 			underline = "None",
 			italic = false,
 			strikethrough = false,
 		},
 		inactive_tab = {
-			fg_color = "silver",
+			fg_color = fg,
 			bg_color = "none",
 		},
 		new_tab = {
-			fg_color = foreground_color,
-			bg_color = background_color,
+			fg_color = fg,
+			bg_color = bg,
 		},
 	},
 }
 
-config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "Regular" })
-config.font_size = 14
-config.initial_cols = 100
-config.initial_rows = 30
-config.line_height = 1.1
+--config.font = wezterm.font("GeistMono Nerd Font", { weight = "Light" })
+-- config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "Light" })
+config.font = wezterm.font("CodeNewRoman Nerd Font", { weight = "Regular" })
+config.font_size = 18
+config.initial_rows = 40
+config.initial_cols = 120
 config.window_decorations = "NONE"
 config.audible_bell = "Disabled"
-config.dpi = 120
 config.hide_tab_bar_if_only_one_tab = true
--- config.tab_bar_at_bottom = true
 
 return config
