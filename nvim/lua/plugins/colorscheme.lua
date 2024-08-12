6a88e7a50e28543005bc623b17bae12591dbe07b
bzmariano <48970081+bzmariano@users.noreply.github.com>
2024-08-12 19:00:57 -0300

    update nvim & clj conf

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,51 +12,28 @@ local alabaster = {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme("alabaster")
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "Normal", { fg = "white", bg = "none" })
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-		vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#aa88ff", bg = "none" })
-		vim.api.nvim_set_hl(0, "@keyword", { fg = "#aa88ff", bg = "none" })
+		vim.api.nvim_set_hl(0, "Whitespace", { fg = "#202020" })
 	end,
 }
 
-local kanagawa = {
-	"rebelot/kanagawa.nvim",
+local onedark = {
+	"navarasu/onedark.nvim",
+	lazy = false,
+	priority = 1000,
 	config = function()
-		vim.o.background = "light"
-		require("kanagawa").setup({
-			compile = false, -- enable compiling the colorscheme
-			undercurl = true, -- enable undercurls
-			commentStyle = { italic = true },
-			functionStyle = {},
-			keywordStyle = { italic = false },
-			statementStyle = { bold = false },
-			typeStyle = { italic = true },
-			transparent = false, -- do not set background color
-			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			colors = { -- add/modify theme and palette colors
-				palette = {},
-				theme = { wave = {}, lotus = {}, dragon = {}, all = { ui = { bg_gutter = "none" } } },
-			},
-			overrides = function(colors) -- add/modify highlights
-				return {}
-			end,
-			theme = "wave", -- Load "wave" theme when 'background' option is not set
-			background = { -- map the value of 'background' option to a theme
-				dark = "wave", -- try "dragon" !
-				light = "lotus",
-			},
+		require("onedark").setup({
+			transparent = true,
 		})
-
-		-- setup must be called before loading
-		vim.cmd("colorscheme kanagawa")
+		require("onedark").load()
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 	end,
 }
 
 return {
-	--kanagawa,
 	alabaster,
+	--onedark,
 }
--- nvim/lua/plugins/lisp.lua
+++ nvim/lua/plugins/lisp.lua
@@ -1,19 +1,66 @@
 local rainbow_delimiters = {
 	"HiPhish/rainbow-delimiters.nvim",
 	lazy = false,
-	ft = { "lisp", "edn", "clj", "cljc" },
 }
 
 local parinfer = {
 	"gpanders/nvim-parinfer",
-	lazy = false,
 	-- ft = { "lisp", "edn", "clj", "cljc" },
+}
+
+local sexp = {
+	"PaterJason/nvim-treesitter-sexp",
+	-- ft = { "lisp", "edn", "clj", "cljc" },
+	config = function()
+		require("treesitter-sexp").setup({
+			-- Enable/disable
+			enabled = true,
+			-- Move cursor when applying commands
+			set_cursor = true,
+			-- Set to false to disable all keymaps
+			keymaps = {
+				-- Set to false to disable keymap type
+				commands = {
+					-- Set to false to disable individual keymaps
+					swap_prev_elem = "<e",
+					swap_next_elem = ">e",
+					swap_prev_form = "<f",
+					swap_next_form = ">f",
+					promote_elem = "<LocalLeader>O",
+					promote_form = "<LocalLeader>o",
+					splice = "<LocalLeader>@",
+					slurp_left = "<(",
+					slurp_right = ">)",
+					barf_left = ">(",
+					barf_right = "<)",
+					insert_head = "<I",
+					insert_tail = ">I",
+				},
+				motions = {
+					form_start = "(",
+					form_end = ")",
+					prev_elem = "[e",
+					next_elem = "]e",
+					prev_elem_end = "[E",
+					next_elem_end = "]E",
+					prev_top_level = "[[",
+					next_top_level = "]]",
+				},
+				textobjects = {
+					inner_elem = "ie",
+					outer_elem = "ae",
+					inner_form = "if",
+					outer_form = "af",
+					inner_top_level = "iF",
+					outer_top_level = "aF",
+				},
+			},
+		})
+	end,
 }
 
 local conjure = {
 	"Olical/conjure",
-	lazy = false,
-	ft = { "lisp", "edn", "clj", "cljc" },
 	dependencies = {
 		{
 			"PaterJason/cmp-conjure",
@@ -98,16 +145,9 @@ local conjure = {
 	end,
 }
 
-local nvlime = {
-	"monkoose/nvlime",
-	dependencies = { "monkoose/parsley" },
-	lazy = false,
-	ft = { "lisp" },
-}
-
 return {
-	parinfer,
-	conjure,
 	rainbow_delimiters,
-	-- nvlime,
+	conjure,
+	parinfer,
+	sexp,
 }
